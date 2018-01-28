05ec7772b73efd8c9a635c1b6ccaa6d550143945
Bob <bobzim17@gmail.com>
2018-01-28 11:54:59 -0600

    initial mini commit

---
+++ conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+bobziemerman:$1$wWG1jID2$Abgfd2VgoqCxbwGJlclpT0:Bob:bobzim17@gmail.com:admin,user
